c67b3bf9a889c068f176c30722c5d1eda65d93fd
Dewi Anggraini Meita Sari <ssitiaisya2@gmail.com>
2026-03-07 22:32:35 +0700

    praktikum4/JS3

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,10 @@
 <?php
 
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [HomeController::class, 'index']);
-
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'food']);
-    Route::get('/beauty-health', [ProductController::class, 'beauty']);
-    Route::get('/home-care', [ProductController::class, 'homecare']);
-    Route::get('/baby-kid', [ProductController::class, 'baby']);
-});
-
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
-
-
-Route::get('/sales', [SalesController::class, 'index']);
+Route::get('/level', [LevelController::class, 'index']);
